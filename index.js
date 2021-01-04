af812ddcdb8e1347e1468d8ed680020348686282
Marcin <marcin.adam.mroczko@gmail.com>
2021-01-04 09:51:08 +0100

    counting

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = 3001
-const handlebars = require('./handlebars');
-var path = require('path');
-const defaultData = require('./default-data');
+const handlebars = require("./handlebars")
+var path = require("path")
+const defaultData = require("./default-data")
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const data = {
     ...defaultData,
     ...req.query
-  };
-  const template = handlebars.template();
-  res.send(template(data));
+  }
+  const template = handlebars.template()
+  res.send(template(data))
 })
 
-app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, "assets")))
+app.use("/js", express.static(__dirname + "/js"))
 
 app.listen(port, () => {
   console.log(`Front end Developer app listening at http://localhost:${port}`)
